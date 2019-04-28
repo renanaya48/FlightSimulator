6b4f1a00810012f84475da6b8dbf5209109278a2
רננה ינובסקי <renanaya@hotmail.com>
2019-04-28 15:30:53 +0300

    change in main window and more

--- FlightSimulator/Views/ChangeCommands.xaml.cs
+++ FlightSimulator/Views/ChangeCommands.xaml.cs
@@ -13,22 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FlightSimulator
+namespace FlightSimulator.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ChangeCommands.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ChangeCommands : UserControl
     {
-        public MainWindow()
+        public ChangeCommands()
         {
             InitializeComponent();
-        }
-
-        
-        private void FlightBoard_Loaded(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
--- FlightSimulator/Views/Window1.xaml.cs
+++ FlightSimulator/Views/Window1.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FlightSimulator
+namespace FlightSimulator.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        
-        private void FlightBoard_Loaded(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
